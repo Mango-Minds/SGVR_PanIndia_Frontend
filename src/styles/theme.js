55f78eeef65170bb3cdc4f7c5e8ddab21c7fff1e
19bcs1564 <19bcs1564@gmail.com>
2025-01-30 13:28:59 +0530

    Updated code

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+  const theme = {
+    themeColor: '#D4AF37',
+    // primaryColor: "#3498db", 
+    // secondaryColor: "#2ecc71",
+    // backgroundColor: "#f5f5f5",
+    // textColor: "#333",
+  };
+  
+  export default theme;
